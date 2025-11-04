d8b30d09c2eda932e5ad04a43fe5505c3eff961a
grexach-zertifier <grexach@zertifier.com>
2025-11-04 10:10:42 +0100

    Inject `CredentialsProvider` and add new components

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,15 +1,25 @@
-import { Component, signal } from '@angular/core';
+import {Component, inject, signal} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { initFlowbite } from 'flowbite';
+import { Lrn } from './components/lrn/lrn';
+import { VpVc } from './components/vp-vc/vp-vc';
+import { LpTc } from './components/lp-tc/lp-tc';
+import { ToastContainer } from './ui/toast-container';
+import {CredentialsProvider} from './core/CredentialsProvider';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Lrn, VpVc, LpTc, ToastContainer],
   templateUrl: './app.html',
+  providers:[
+    CredentialsProvider
+  ],
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('zertifier-signerapp');
+
+  // credentialsProvider = inject(CredentialsProvider);
   ngOnInit(): void {
     initFlowbite();
   }
