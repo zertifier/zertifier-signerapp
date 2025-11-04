0710a753ae51677148ba15a7db268f5df11b38d2
grexach-zertifier <grexach@zertifier.com>
2025-11-04 10:10:53 +0100

    Inject `CertificateProvider` and `CredentialsProvider`

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,11 +2,17 @@ import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChang
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import {provideHttpClient} from '@angular/common/http';
+import {CertificateProvider} from './core/CertificateProvider';
+import {CredentialsProvider} from './core/CredentialsProvider';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes)
+    provideRouter(routes),
+    provideHttpClient(),
+    CertificateProvider,
+    CredentialsProvider
   ]
 };
