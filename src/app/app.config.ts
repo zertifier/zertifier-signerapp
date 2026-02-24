34eec48be564490db73f20e352228b60f3972ad4
Ilia <rain-ilia@rambler.ru>
2026-02-24 14:24:06 +0100

    some bs cleanup after moving files

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,8 +3,8 @@ import {provideRouter} from '@angular/router';
 
 import {routes} from './app.routes';
 import {provideHttpClient, withFetch, withInterceptors} from '@angular/common/http';
-import {retryInterceptor} from './services/http.retry.interceptor';
-import {loggingInterceptor} from './services/http.logging.interceptor';
+import {retryInterceptor} from './services/interceptors/http.retry.interceptor';
+import {loggingInterceptor} from './services/interceptors/http.logging.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
