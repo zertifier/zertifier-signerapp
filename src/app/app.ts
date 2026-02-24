5023eb3252f8eb9ac30e624a8b3a298608411480
Ilia <rain-ilia@rambler.ru>
2026-02-24 14:37:10 +0100

    interface was not implemented

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,8 +1,7 @@
-import {ChangeDetectionStrategy, Component, inject, signal} from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { initFlowbite } from 'flowbite';
-import { ToastContainer } from './ui/toast-container';
-import {CredentialsProvider} from './core/CredentialsProvider';
+import {ChangeDetectionStrategy, Component, OnInit, signal} from '@angular/core';
+import {RouterOutlet} from '@angular/router';
+import {initFlowbite} from 'flowbite';
+import {ToastContainer} from './ui/toast-container';
 import {ComponentContainer} from './components/component-container/component-container';
 
 @Component({
@@ -12,11 +11,11 @@ import {ComponentContainer} from './components/component-container/component-con
   styleUrl: './app.css',
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class App {
-  protected readonly title = signal('zertifier-signerapp');
-
+export class App implements OnInit {
   // Controls visibility of the bottom debug/test area
   readonly showTestArea = signal(false);
+  protected readonly title = signal('zertifier-signerapp');
+
   toggleTestArea(): void {
     this.showTestArea.update(v => !v);
   }
