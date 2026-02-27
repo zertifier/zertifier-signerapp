43ba8e2926500a08182e11d21b1b8b82c7f425bc
Ilia <rain-ilia@rambler.ru>
2026-02-27 14:39:38 +0100

    router

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,37 @@
-import { Routes } from '@angular/router';
+import {Routes} from '@angular/router';
+import {MainWindow} from './components/main-window/main-window';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: MainWindow,
+    children: [
+      {
+        path: '',
+        redirectTo: 'compliance',
+        pathMatch: 'full'
+      },
+      {
+        path: 'compliance',
+        // Using lazy loading for the child components
+        loadComponent: () => import('./components/compliance/compliance').then(m => m.Compliance)
+      },
+      {
+        path: 'lnr',
+        // Using lazy loading for the child components
+        loadComponent: () => import('./components/legal-registration-number/legal-registration-number').then(m => m.LegalRegistrationNumber)
+      },
+      {
+        path: 'lp',
+        // Using lazy loading for the child components
+        loadComponent: () => import('./components/legal-participant/legal-participant').then(m => m.LegalParticipant)
+      },
+      {
+        path: 'tac',
+        // Using lazy loading for the child components
+        loadComponent: () => import('./components/terms-and-condtions/terms-and-condtions').then(m => m.TermsAndCondtions)
+      }
+    ]
+
+  },
+];
