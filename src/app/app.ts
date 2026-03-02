2d46ff4c23cff80af52d2a12450b9815aed7a7d5
Ilia <rain-ilia@rambler.ru>
2026-03-02 15:06:11 +0100

    activating toasts

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,11 @@
 import {ChangeDetectionStrategy, Component, OnInit} from '@angular/core';
 import {RouterOutlet} from '@angular/router';
 import {initFlowbite} from 'flowbite';
+import {ToastContainer} from './ui/toast-container';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,],
+  imports: [RouterOutlet, ToastContainer,],
   templateUrl: './app.html',
   styleUrl: './app.css',
   changeDetection: ChangeDetectionStrategy.OnPush
