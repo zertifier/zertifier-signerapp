b09f59a1cd91a3b2992740ddf760b47d965e472b
Jose David <josedvd123@gmail.com>
2026-03-03 14:35:20 +0100

    interceptor logs with query

--- src/app/services/interceptors/http.logging.interceptor.ts
+++ src/app/services/interceptors/http.logging.interceptor.ts
@@ -2,11 +2,11 @@ import {HttpEvent, HttpEventType, HttpHandlerFn, HttpRequest} from '@angular/com
 import {Observable, tap} from 'rxjs';
 
 export function loggingInterceptor(req: HttpRequest<unknown>, next: HttpHandlerFn): Observable<HttpEvent<unknown>> {
-  console.log(' --> Fetching:', req.method, req.url, req.body ? req.body : '');
+  console.log(' --> Fetching:', req.method, req.urlWithParams, req.body ? req.body : '');
   return next(req).pipe(
     tap(event => {
       if (event.type === HttpEventType.Response) {
-        console.log(' <-- Response:', req.url, event.body);
+        console.log(' <-- Response:', req.urlWithParams, event.body);
       }
     }));
 }
