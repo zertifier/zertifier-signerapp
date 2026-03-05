306b49497651567a326da91bf2bce82a4566856c
Ilia <rain-ilia@rambler.ru>
2026-03-05 09:09:12 +0100

    token added to header for publishing server

--- src/app/services/publishers/PublishService.ts
+++ src/app/services/publishers/PublishService.ts
@@ -1,12 +1,19 @@
 import {inject, Injectable} from '@angular/core';
 import {BaseFilePublisher} from './HttpPublisher';
 import {PublishedFile} from '../../core/types/publisher.types';
+import {environment} from '../../../environments/environment';
+import {HttpHeaders} from '@angular/common/http';
 
 @Injectable()
 export class PublishService {
   #httpPublisher = inject(BaseFilePublisher);
 
   publish(files: PublishedFile[], endpointUrl: string) {
-    return this.#httpPublisher.publish(files, endpointUrl);
+    const token = environment.zertifierFileApiToken;
+    if (!token) {
+      throw new Error('No token found in .env for publish service')
+    }
+    const header = new HttpHeaders().set('Authorization', `Bearer ${token}`);
+    return this.#httpPublisher.publish(files, endpointUrl, header);
   }
 }
