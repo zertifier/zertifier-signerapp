06a99a68aebdff4662ea6f2e2e26aae42d68b413
Ilia <rain-ilia@rambler.ru>
2026-03-26 08:54:05 +0100

    result now have a copy button and accepts strings

--- src/app/ui/result-block/result-block.ts
+++ src/app/ui/result-block/result-block.ts
@@ -1,5 +1,6 @@
-import {Component, input} from '@angular/core';
+import {Component, inject, input} from '@angular/core';
 import {JsonPipe} from "@angular/common";
+import {ToastService} from '../../services/ToastService';
 
 @Component({
   selector: 'app-result-block',
@@ -10,5 +11,18 @@ import {JsonPipe} from "@angular/common";
   styleUrl: './result-block.css',
 })
 export class ResultBlock {
-  content = input<Object | null | undefined>();
+  content = input<any>();
+  #toast = inject(ToastService);
+
+  copyToClipboard() {
+    if (!this.content()) {
+      this.#toast.error("Content to copy not found.", {duration: 2000});
+      return;
+    }
+    navigator.clipboard
+      .writeText(typeof this.content() === 'string' ? this.content() : JSON.stringify(this.content(), null, 2))
+      .then(() => {
+        this.#toast.info('Copied!', {duration: 2000});
+      });
+  }
 }
