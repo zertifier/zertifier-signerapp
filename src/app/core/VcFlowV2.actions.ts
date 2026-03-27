abd8a0bebf738646a3de20c3594f8a9483066664
Ilia <rain-ilia@rambler.ru>
2026-03-27 14:41:29 +0100

    even better headers

--- src/app/core/VcFlowV2.actions.ts
+++ src/app/core/VcFlowV2.actions.ts
@@ -52,7 +52,7 @@ export class VcFlowV2Actions {
   signVp(pKey: CryptoKey, did: string, vcid: string, jwsArr: string[]) {
     return this.#signVC(pKey, this.#credBuilder.vp(did, vcid, jwsArr), did, {
       cty: "vp+ld+json",
-      typ: "vp+ld+json+jwt"
+      typ: "vp+ld+jwt"
     });
   }
 
